7b478f4e0a07d1cbae6596733e5faa1bd9d3f545
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-06-27 01:41:01 +0300

    📝 Refactoring Search component

--- src/components/shared/Search.tsx
+++ src/components/shared/Search.tsx
@@ -1,7 +1,15 @@
+import { CancelIcon } from '@/components/shared/Icons';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import { GetAllQueryParams } from '@/types/global.types';
-import React, { Dispatch, SetStateAction, useCallback } from 'react';
+import React, {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 interface SearchProps<T extends GetAllQueryParams>
   extends React.ComponentProps<typeof Input> {
   setParams: Dispatch<SetStateAction<T>>;
@@ -11,27 +19,66 @@ const Search = <T extends GetAllQueryParams>({
   setParams,
   ...props
 }: SearchProps<T>) => {
-  const handleSearch = useCallback(
+  const [keyword, setKeyword] = useState<string>('');
+  const [isFocused, setIsFocused] = useState(false);
+  const handleOnChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
-      setParams((prev) => ({
-        ...prev,
-        keyword: e.target.value,
-        limit: prev.limit,
-        page: 1,
-      }));
+      setKeyword(e.target.value);
     },
-    [setParams],
+    [],
   );
+  const clear = useCallback(() => {
+    setKeyword('');
+  }, []);
+
+  const handleOnFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+  const handleOnBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
+  useEffect(() => {
+    setParams((prev) => ({
+      ...prev,
+      keyword: keyword.trim(),
+      limit: prev.limit,
+      page: 1,
+    }));
+  }, [keyword, setParams]);
+
   return (
-    <Input
+    <div
       className={cn(
-        'w-full focus:border-primary focus-visible:ring-0 focus-visible:ring-transparent md:w-[300px]',
-        className,
+        'flex items-center justify-between gap-2 rounded border px-4 md:w-fit',
+        {
+          'border-primary': isFocused,
+        },
       )}
-      placeholder="Search"
-      onChange={handleSearch}
-      {...props}
-    />
+    >
+      <Input
+        className={cn(
+          'w-full border-none p-0 focus-visible:border-none focus-visible:ring-transparent focus-visible:ring-offset-0 md:w-[300px]',
+          className,
+        )}
+        placeholder="Search"
+        onChange={handleOnChange}
+        onFocus={handleOnFocus}
+        onBlur={handleOnBlur}
+        value={keyword}
+        {...props}
+      />
+      {keyword.length > 0 && (
+        <Button
+          size={'icon'}
+          variant={'ghost'}
+          className="size-auto"
+          onClick={clear}
+        >
+          <CancelIcon size={16} />
+        </Button>
+      )}
+    </div>
   );
 };
 
